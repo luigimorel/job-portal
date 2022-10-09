bda6330a76dd739054f00fada8ac5138107df63f
Luigi Morel <morelmiles@gmail.com>
2022-10-09 17:57:18 +0300

    add an interface store

---
+++ web/src/interfaces/job-interface.ts
@@ -0,0 +1,14 @@
+export default interface JobProps {
+  jobs: [
+    id: number,
+    title: string,
+    logo: string,
+    company: string,
+    continent: string,
+    type: string,
+    duration: string,
+    industry: string,
+    location_of_work_place: string,
+    date_posted: string
+  ];
+}

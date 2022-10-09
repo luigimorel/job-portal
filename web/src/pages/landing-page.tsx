02b61d915d18dc202e57ec90148bc0c70fbea0a6
Luigi Morel <morelmiles@gmail.com>
2022-10-09 17:57:47 +0300

    fetch job information from api

--- web/src/pages/landing-page.tsx
+++ web/src/pages/landing-page.tsx
@@ -1,13 +1,24 @@
+import React, { useEffect } from "react";
+
 import { Footer } from "../components/footer";
 import { Hero } from "../components/hero";
 import { Job } from "../components/job";
 import { Newsletter } from "../components/newsletter";
+import { getAllJobs } from "../services/jobservice";
 
 export const LandingPage = () => {
+  const [jobs, setJobs] = React.useState([]);
+
+  useEffect(() => {
+    getAllJobs().then((jobs) => {
+      setJobs(jobs);
+    });
+  }, []);
+
   return (
     <>
       <Hero />
-      <Job />
+      <Job jobs={jobs} />
       {/* <Partners /> */}
       <Newsletter />
       <Footer />

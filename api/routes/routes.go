693ba860dae4702420eed272b5e02a4104fb8e29
Luigi Morel <morelmiles@gmail.com>
2022-10-09 17:58:50 +0300

    add CORS handler

--- api/routes/routes.go
+++ api/routes/routes.go
@@ -5,11 +5,14 @@ import (
 
 	"github.com/gorilla/mux"
 	"github.com/morelmiles/startupjobs/controllers"
+	"github.com/rs/cors"
 )
 
 func Routes() {
 
 	router := mux.NewRouter()
+	corsHandler := cors.Default().Handler(router)
+
 	router.HandleFunc("/", controllers.Home).Methods("GET")
 
 	// Jobs
@@ -18,5 +21,5 @@ func Routes() {
 	router.HandleFunc("/api/v1/create", controllers.CreateJob).Methods("POST")
 	router.HandleFunc("/api/v1/job/{id}", controllers.DeleteJobById).Methods("DELETE")
 	router.HandleFunc("/api/v1/job/{id}", controllers.UpdateJobById).Methods("PUT")
-	http.ListenAndServe(":8080", router)
+	http.ListenAndServe(":8080", corsHandler)
 }

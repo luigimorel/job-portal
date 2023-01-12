01e0b4cd51f033037f793066c6661a44fc402fd8
Luigi Morel <morelmiles@gmail.com>
2023-01-12 14:53:22 +0300

    fix: update routes
    Signed-off-by: Luigi Morel <morelmiles@gmail.com>

--- api/routes/routes.go
+++ api/routes/routes.go
@@ -19,9 +19,9 @@ func Routes() {
 
 	// Jobs
 	router.HandleFunc("/api/v1/jobs", controllers.GetJobs).Methods("GET")
-	router.HandleFunc("/api/v1/job/{id}", controllers.GetJobById).Methods("GET")
+	router.HandleFunc("/api/v1/jobs/{id}", controllers.GetJobById).Methods("GET")
 	router.HandleFunc("/api/v1/create", controllers.CreateJob).Methods("POST")
-	router.HandleFunc("/api/v1/job/{id}", controllers.DeleteJobById).Methods("DELETE")
-	router.HandleFunc("/api/v1/job/{id}", controllers.UpdateJobById).Methods("PUT")
+	router.HandleFunc("/api/v1/jobs/{id}", controllers.DeleteJobById).Methods("DELETE")
+	router.HandleFunc("/api/v1/jobs/{id}", controllers.UpdateJobById).Methods("PUT")
 	http.ListenAndServe(":"+port, corsHandler)
 }
